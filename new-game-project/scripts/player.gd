a57301483ea13336cb5447822c3b14c60564a579
Flap <shuruiguo1@gmail.com>
2025-10-09 23:09:05 +1100

    jumping, gravity tilemap
    jump mechanic, gravity and testing out tilemap + collision

--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
-
-const speed = 300
-const jumpheight = -100
-func _physics_process(_delta):
+var gravity = 10000
+var jump = -20000
+const speed = 400
+func _physics_process(delta):
 	velocity.x = 0
 	velocity.y = 0
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 	elif Input.is_action_pressed("move_left"):
 		velocity.x = -speed
-	if Input.is_action_pressed("jump"):
-		velocity.y = jumpheight
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump
 	move_and_slide()
